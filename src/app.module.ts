f0bce4738707050a9a247ddcd4f1eae39cd9efb0
natali <cassiopia@yandex.ru>
2022-01-31 22:45:55 +0300

    Day 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,22 @@
 import {Module} from "@nestjs/common";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import {SequelizeModule} from "@nestjs/sequelize";
 
-@Module({})
+@Module({
+    controllers:[AppController],
+    providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'admin',
+            password: '1',
+            database: 'training-node-js',
+            models: [],
+            autoLoadModels: true
+        }),
+    ],
+})
 export class AppModule{}
